9a64bf6078d76f1ed6610ba2743f18fe20e46693
henrybos09 <henryboston@6sboston.com>
2026-02-09 14:46:32 -0800

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -30,7 +30,7 @@ export const defaultConfig: Config = {
     textColor: '#1F2937',
   },
   turnstile: {
-    siteKey: '0x4AAAAAABaeDHdwHVmJobfE',
+    siteKey: '0x4AAAAAACZz1hAT65M15Umx',
     maxAttempts: 5,
     cooldownPeriod: 3600000 // 1 hour
   }
